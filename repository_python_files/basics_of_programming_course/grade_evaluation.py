9922cd74db756017b727252e8c2c29f6e3760334
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-11-03 13:30:35 +0100

    collected python files from different repositories and for different tasks

---
+++ repository_python_files/basics_of_programming_course/grade_evaluation.py
@@ -0,0 +1,17 @@
+def evaluation(points):
+    if points <= 54:
+        return 5
+
+    if points <= 64:
+        return 6
+
+    if points <= 74:
+        return 7
+
+    if points <= 84:
+        return 8
+
+    if points <= 94:
+        return 9
+
+    return 10
