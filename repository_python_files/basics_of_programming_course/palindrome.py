7a1eaaf7055a34a6de94d601b0ff549b2fde6e6d
Nemanja Vujadinovic <96585470+vujadinovicn@users.noreply.github.com>
2024-11-04 15:52:42 +0100

    Delete repository_python_files directory

--- repository_python_files/basics_of_programming_course/palindrome.py
+++
@@ -1,7 +0,0 @@
-def palindrome(text):
-    text = text.lower()
-    is_palindrome = True
-    for i in range(len(text) // 2):
-        if text[i] != text[-1 - i]:
-            is_palindrome = False
-    return is_palindrome
--- repository_python_files/basics_of_programming_course/quadratic.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-
-def quadratic_equation(a, b, c):
-    D = b**2-4*a*c
-    if D>0:
-        square_root = sqrt(D)
-        x1 = (-b+square_root)/(2*a)
-        x2 = (-b-square_root)/(2*a)
-        return x1, x2
